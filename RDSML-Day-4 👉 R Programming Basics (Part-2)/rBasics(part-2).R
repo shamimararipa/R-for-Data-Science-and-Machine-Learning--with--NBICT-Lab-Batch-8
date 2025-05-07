74cccee86b87bf1cdcfbc9a2bfd4b1c1dac660aa
shamimararipa <shamimararipa40751@gmail.com>
2025-05-07 22:50:36 +0600

    Day-2 Practice

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasics(part-2).R
@@ -0,0 +1,47 @@
+# Data Types in R
+# 1. Numeric - Integer / Floating Point 
+# 2. String/Character- "Ms. Shamim Ara Ripa" 
+# 3. Logical
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = "Ms. Shamim Ara Ripa"
+print(name)
+class(name)
+
+c = "52"
+print(c+9)
+print(a+9)
+class(c)
+
+# Logical Data Type - TRUE, FALSE (T, F)
+print(a<b)
+x = 19
+y = 22-3
+print(x==y)
+print(x !=y)
+print(y >=x)
+
+# Logical operators in R
+# && Logical operator
+# || Logical operator
+4 ==4 && 5 ==5
+x != y || x == y
+
+
+# Simple Mathematical Operators in R
+z = 6 + 9 - 8 * 2 / 3
+print(z)
+
+w = (4 + 3 - 3) * 2 / 3
+print(w)
+
+y = 4^3
+print(y)
+
+11 / 5
+11 %% 5
+11 %/% 5
