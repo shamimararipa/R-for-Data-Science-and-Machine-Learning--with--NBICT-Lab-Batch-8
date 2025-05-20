6230edab5ed31147d4a227eb19f78bb6c1f4e96b
shamimararipa <shamimararipa40751@gmail.com>
2025-05-20 09:04:32 +0600

    Hw_day1.R

---
+++ Practice file/HW Day 1/Hw_day 1. R.R
@@ -0,0 +1,14 @@
+# Updating the file
+data = c(4, 10, 18, 15, 20, 16, 14, 22, 15)
+data
+x = mean(data)
+x
+summary(data)
+z=x/2
+z
+z = x/2
+z
+data_2 = c(10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40)
+a = mean(data_2)
+a
+summary(data_2)
