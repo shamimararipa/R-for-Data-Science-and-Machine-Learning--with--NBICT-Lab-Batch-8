141c54f7c73e7bc4b633ae12245ed7ad90d1f742
shamimararipa <shamimararipa40751@gmail.com>
2025-05-21 23:00:48 +0600

    descriptive.R

---
+++ RDSML-Day-8 👉 Descriptive Statistics in R/Descriptive.R
@@ -0,0 +1,21 @@
+# Descriptive statistics using the psych package
+
+install.packages("psych")
+library(psych)
+
+data()
+AirPassengers 
+BOD
+
+# Using the New York airport flight data
+install.packages("nycflights13")
+attach(nycflights13::flights)
+
+# Lets look at the first 6 records using the head() function
+head(nycflights13::flights)
+summary(distance)
+describe(distance)
+
+demo = cbind(arr_delay, dep_delay, distance)
+demo
+describe(demo)
