f97230f8a5362eccdfa3f5b85a00a8fdf8cb41f5
shamimararipa <shamimararipa40751@gmail.com>
2025-06-05 21:59:01 +0600

    hw_day 3-4

---
+++ Practice file/HW Day 3-4/hw_day 3-4.R
@@ -0,0 +1,36 @@
+# calculating some health parameter
+
+my_age = 97
+my_weight = 60
+my_height = 65
+
+x = (my_height * my_age) / my_weight
+print(x)
+
+students_height = c(50, 55, 64, 71, 45.25)
+mean(students_height)
+
+# Data types in R
+# 1. Numeric - Integer/Floating point
+# 2. string/Character - "Ms. Shamim Ara Ripa"
+# 3. Logical
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = "Ms. Shamim Ara Ripa"
+print(name)
+class(name)
+c = "52"
+print(c+9)
+print(a+9)
+class(c)
+
+# Logical data type - TRUE, FALSE (T,F)
+print(a<b)
+x = 19
+y = 22-3
+print(x==y)
+print(x !=y)
